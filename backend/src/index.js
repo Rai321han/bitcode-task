2a6778db0ed3697e0943d452920ae23253c7e73f
Raihan Uddin <uddinraihan797@gmail.com>
2025-06-23 17:51:28 +0600

    add auth routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import roadmapRouter from "./routes/roadmap.routes.js";
 import commentsRouter from "./routes/comments.route.js";
 import cors from "cors";
+import authRouter from "./routes/auth.routes.js";
 
 dotenv.config();
 const app = express();
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", roadmapRouter);
 app.use("/api", commentsRouter);
+app.use("/api", authRouter);
 
 const port = process.env.BACKEND_PORT || 5100;
 app.listen(port, () => {
