4c217dc60f12aeaced258d159c8e5d3634eeefa0
Raihan Uddin <uddinraihan797@gmail.com>
2025-06-23 17:51:00 +0600

    add user schema

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      min: [3, "Username cannot be empty!"],
+      max: [40, "Too many characters"],
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      min: [6, "Password must be at least 6 characters long"],
+      select: false, // exclude from query results by default
+    },
+    email: {
+      required: [true, "Email is required!"],
+      type: String,
+      unique: true,
+      min: [1, "Email is required!"],
+    },
+    refreshToken: {
+      required: true,
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", UserSchema);
+export default User;
