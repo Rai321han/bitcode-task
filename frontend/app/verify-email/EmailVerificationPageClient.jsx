8d191abcf3d37ed4e695b0efa1011899c667a709
Raihan Uddin <uddinraihan797@gmail.com>
2025-06-24 20:51:01 +0600

    fix: useSerachParams wrapped in suspense

---
+++ frontend/app/verify-email/EmailVerificationPageClient.jsx
@@ -0,0 +1,42 @@
+"use client";
+import { useSearchParams, useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function EmailVerificationPage() {
+  const params = useSearchParams();
+  const token = params.get("token");
+  const router = useRouter();
+
+  useEffect(() => {
+    async function verify() {
+      if (!token) return;
+
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_API_URL}/verify-email?token=${token}`
+        );
+        const data = await res.json();
+
+        if (!res.ok) {
+          console.error(data.message);
+          console.error(data.message);
+          alert("Email verification failed: " + data.message);
+          router.push("/error");
+          return;
+        }
+
+        router.push("/roadmaps");
+      } catch (err) {
+        console.log("Error: ", err);
+      }
+    }
+
+    verify();
+  }, [token, router]);
+
+  return (
+    <div className="text-center py-10">
+      <h1 className="text-xl font-bold">Verifying your email...</h1>
+    </div>
+  );
+}
--- frontend/app/verify-email/page.js
+++ frontend/app/verify-email/page.js
@@ -1,43 +1,10 @@
-"use client";
-import { useSearchParams, useRouter } from "next/navigation";
-import { useEffect } from "react";
-import { useAuth } from "../providers/AuthProvider";
+import { Suspense } from "react";
+import EmailVerificationPage from "./EmailVerificationPageClient";
 
-export default function EmailVerificationPage() {
-  const params = useSearchParams();
-  const token = params.get("token");
-  const router = useRouter();
-
-  useEffect(() => {
-    async function verify() {
-      if (!token) return;
-
-      try {
-        const res = await fetch(
-          `http://localhost:5100/api/verify-email?token=${token}`
-        );
-        const data = await res.json();
-
-        if (!res.ok) {
-          console.error(data.message);
-          console.error(data.message);
-          alert("Email verification failed: " + data.message);
-          router.push("/error");
-          return;
-        }
-
-        router.push("/roadmaps");
-      } catch (err) {
-        console.log("Error: ", err);
-      }
-    }
-
-    verify();
-  }, [token, router]);
-
+export default function VerifyEmailPage() {
   return (
-    <div className="text-center py-10">
-      <h1 className="text-xl font-bold">Verifying your email...</h1>
-    </div>
+    <Suspense fallback={<div className="text-center py-10">Loading...</div>}>
+      <EmailVerificationPage />
+    </Suspense>
   );
 }
