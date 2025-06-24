110851ddc309a0d657b2a16ec0aa7dbd5d9ca567
Raihan Uddin <uddinraihan797@gmail.com>
2025-06-24 20:54:51 +0600

    fix: useSerachParams wrapped in suspense

--- frontend/app/verify-reminder/page.js
+++ frontend/app/verify-reminder/page.js
@@ -1,18 +1,10 @@
-"use client";
-import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import VerifyReminderPageClient from "./VerifyEmailPageClient"; // Move your code to this file
 
-export default function VerifyEmailPage() {
-  const params = useSearchParams();
-  const email = params.get("email");
-
+export default function VerifyReminderPage() {
   return (
-    <div className="text-center py-10">
-      <h1 className="text-2xl font-bold">Verify Your Email</h1>
-      <p className="text-gray-600 mt-4">
-        A verification email has been sent to{" "}
-        <span className="font-semibold">{email}</span>. <br />
-        Please check your inbox and click the link to activate your account.
-      </p>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <VerifyReminderPageClient />
+    </Suspense>
   );
 }
